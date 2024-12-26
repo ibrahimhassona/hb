488942aafabb2a2c2dd599934fee7f0dbac3e373
ibrahimhassona <ibrahemhasson@gmail.com>
2024-12-26 18:11:51 +0300

    Single Page variantes v1

--- components/product/ProductSelect.jsx
+++ components/product/ProductSelect.jsx
@@ -3,16 +3,20 @@ import VariantSelect from './VariantSelect';
 import { useTranslations } from 'next-intl';
 
 
-const ProductSelect = ({ product }) => {
+const ProductSelect = ({ product, dataAttributes, onVariantSelect }) => {
     const t = useTranslations("product")
+    const sizes = dataAttributes?.filter(item => item.Variant_Type == "size")
+    const colors = dataAttributes?.filter(item => item.Variant_Type == "color")
+    // console.log("sizes ======>", sizes)
+    // console.log("colors ======>", colors)
     return (
         <div className="grid grid-cols-2 items-center gap-2 mb-4">
             {/* Capacity Select */}
-            {product?.capacity.length > 0 && <VariantSelect title={t("size")} data={product?.capacity} />}
+            {sizes?.length > 0 && <VariantSelect title={t("size")} data={sizes} onVariantSelect={onVariantSelect} />}
             {/* Size Select */}
-            {product?.size.length > 0 && <VariantSelect title={t("capacity")} data={product?.size} />}
+            {colors?.length > 0 && <VariantSelect title={t("color")} data={colors} onVariantSelect={onVariantSelect} />}
         </div >
     );
 };
 
-export default ProductSelect ;
+export default ProductSelect;
